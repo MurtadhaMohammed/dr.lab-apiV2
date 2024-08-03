833f7b5769fb0375ae23a1ad0434f27b1d95bcc8
MurtadhaMohammed <mumu9943@gmail.com>
2024-08-03 13:04:33 +0300

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3000;
 const adminRouter = require("./routers/admin");
 const clientRouter = require("./routers/client");
 const whatsappRouter = require("./routers/whatsapp");
+const featureRouter = require("./routers/feature");
 const fileUpload = require("express-fileupload");
 
 require("dotenv").config();
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 app.use("/api/admin", adminRouter);
 app.use("/api/client", clientRouter);
 app.use("/api/send", whatsappRouter);
+app.use("/api/feature", featureRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

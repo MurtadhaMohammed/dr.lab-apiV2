e1dcb4887e81bace6098681d4a3aeacb3f1244a2
GTYaseen <YaseenTahaAlEnezi@gmail.com>
2025-05-04 13:33:43 +0300

    edit

--- helper/generateToken.js
+++ helper/generateToken.js
@@ -1,11 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (client) => {
+  console.log(client);
+  
   return jwt.sign(
     {
       id: client.id,
       username: client.username,
       device: client.device,
+      labName: client.labName,
+      fullName: client.name,
+      email: client.email,
+      phone: client.phone,
+      plan: client.plan,
+      address: client.address
     },
     process.env.JWT_SECRET,
     { expiresIn: "1y" } 
